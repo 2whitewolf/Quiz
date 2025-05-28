5b2e42248e1af32aefa2d3bea065c24d902dac6b
Bogdan Sevcenco <48834435+2whitewolf@users.noreply.github.com>
2025-05-28 13:44:23 +0300

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Bogdan on 28.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
